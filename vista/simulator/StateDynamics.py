490b4564ddb408733c0a48353ed1a5ffc9399a03
Alexander Amini <xan.amini@gmail.com>
2020-12-04 01:23:04 -0500

    cleanup and minor refactoring of view synthesis to remove tensorflow dependencies within vista (still needed outside for training)

--- vista/simulator/StateDynamics.py
+++ vista/simulator/StateDynamics.py
@@ -1,35 +1,38 @@
 import numpy as np
 
-class StateDynamics(object):
 
-    def __init__(self, translational_state=0.0, longitudinal_state=0.0, theta_state=0.0):
-        # Correct x/y?
+class StateDynamics(object):
+    def __init__(self,
+                 translational_state=0.0,
+                 longitudinal_state=0.0,
+                 theta_state=0.0):
+
         self.x_state = translational_state
         self.y_state = longitudinal_state
         self.theta_state = theta_state
 
     def reset(self):
-        self.theta_state = 0 # used to build R matrix
+        self.theta_state = 0  # used to build R matrix
         self.x_state = 0
         self.y_state = 0
 
     def step(self, curvature, velocity, delta_t):
         arc_length = abs(velocity * delta_t)
-        theta = arc_length * curvature # angle of traversed circle
+        theta = arc_length * curvature  # angle of traversed circle
 
         # Compute R
         self.theta_state += theta
         c = np.cos(self.theta_state)
         s = np.sin(self.theta_state)
-        R = np.array([[c,0,-s], [0,1,0], [s,0,c]])
+        R = np.array([[c, 0, -s], [0, 1, 0], [s, 0, c]])
 
         # Compute local x, y positions
-        x = (1-np.cos(theta))/curvature
-        y = np.sin(theta)/curvature
+        x = (1 - np.cos(theta)) / curvature
+        y = np.sin(theta) / curvature
 
         # Transform positions from local to global
-        R_2 = np.array([[c, -s],[s, c]])
-        xy_local = np.array([[x],[y]])
+        R_2 = np.array([[c, -s], [s, c]])
+        xy_local = np.array([[x], [y]])
         [[x_global], [y_global]] = np.matmul(R_2, xy_local)
 
         self.x_state += x_global
@@ -47,8 +50,6 @@ class StateDynamics(object):
             theta_state=self.theta_state)
 
     def __repr__(self):
-        return "<{}: [{}, {}, {}]>".format(
-            self.__class__.__name__,
-            self.x_state,
-            self.y_state,
-            self.theta_state)
+        return "<{}: [{}, {}, {}]>".format(self.__class__.__name__,
+                                           self.x_state, self.y_state,
+                                           self.theta_state)
--- vista/simulator/World.py
+++ vista/simulator/World.py
@@ -13,49 +13,62 @@ from .util import LabelSearch, get_synced_labeled_timestamps
 from .util import MultiFrame
 from .util import TopicNames
 from .util import print_util as P
+from .util import ViewSynthesis
 
 
 class World:
-    def __init__(self, trace_path, sess, which_camera, camera, VS):
+    def __init__(self, trace_path, which_camera, camera):
         self.trace_path = trace_path
 
         # Initialize configuration variables
         ''' Labels of data (can search with regex) '''
         self.labels = [
-            LabelSearch('day|night', 'dry|rain|snow', 'local|residential|highway|unpaved|indoor', 'stable', '.*', '.*', 1 ),
+            LabelSearch('day|night', 'dry|rain|snow',
+                        'local|residential|highway|unpaved|indoor', 'stable',
+                        '.*', '.*', 1),
         ]
         self.road_width = 4
         self.car_width = 2
 
         # Create camera and View Synthesis objects
-        self.sess = sess
         self.which_camera = which_camera
         self.camera = camera
-        self.view_synthesizer = VS
 
         # Setup variables
+        self.view_synthesizer = ViewSynthesis(self.camera).synthesize
         self.syncedLabeledFrames = self.spawn(trace_path)
         self.masterClock = MultiFrame(trace_path)
         self.current_frame_index = 0
         self.current_trace_index = 0
         self.isCrashed = False
         self.trace_done = False
-        self.syncedLabeledTimestamps = [] # Create synced and labeled timestamps for each trace
+
+        # Create synced and labeled timestamps for each trace
+        self.syncedLabeledTimestamps = []
         self.num_of_frames = 0
         for trace in self.syncedLabeledFrames:
             trace_timestamps = []
             for frame_num in trace[self.which_camera]:
-                trace_timestamps.append(self.masterClock.get_time_from_frame_num(self.which_camera,frame_num))
+                trace_timestamps.append(
+                    self.masterClock.get_time_from_frame_num(
+                        self.which_camera, frame_num))
             self.syncedLabeledTimestamps.append(trace_timestamps)
             self.num_of_frames += len(trace_timestamps)
-        self.f_position, self.f_speed, self.f_curvature = self.get_interp_functions()
-        self.curv_reset_probs = self.get_curv_reset_probs(self.current_trace_index, self.f_curvature)
+
+        # Setup some state information functions and reset probabilities
+        (self.f_position, self.f_speed, self.f_curvature) = \
+            self.get_interp_functions()
+        self.curv_reset_probs = self.get_curv_reset_probs(
+            self.current_trace_index, self.f_curvature)
 
         # Initialize stream
-        video = os.path.join(trace_path, self.which_camera+'.avi')
-        self.stream = FFReader(video, custom_size=(self.camera.get_height(),self.camera.get_width()), verbose=False)
+        video = os.path.join(trace_path, self.which_camera + '.avi')
+        self.stream = FFReader(
+            video,
+            custom_size=(self.camera.get_height(), self.camera.get_width()),
+            verbose=False)
 
-        self.reset() # Randomly resets to a trace in provided path
+        self.reset()  # Randomly resets to a trace in provided path
 
     def reset(self):
         """ Resets the environment due to the end of a RL episode. """
@@ -63,15 +76,23 @@ class World:
 
         # Find index of next trace
         self.current_trace_index = self.find_trace_reset()
-        self.curv_reset_probs = self.get_curv_reset_probs(self.current_trace_index, self.f_curvature) # Get new curv reset probs for it
+        self.curv_reset_probs = self.get_curv_reset_probs(
+            self.current_trace_index,
+            self.f_curvature)  # Get new curv reset probs for it
 
         # Reset favoring places with higher curvatures
-        self.current_frame_index = self.find_curvature_reset(self.curv_reset_probs)
+        self.current_frame_index = self.find_curvature_reset(
+            self.curv_reset_probs)
 
         # First timestamp of trace
-        self.first_time = self.masterClock.get_time_from_frame_num(self.which_camera, self.syncedLabeledFrames[self.current_trace_index][self.which_camera][self.current_frame_index]) # MODIFIED
+        self.first_time = self.masterClock.get_time_from_frame_num(
+            self.which_camera,
+            self.syncedLabeledFrames[self.current_trace_index][
+                self.which_camera][self.current_frame_index])  # MODIFIED
         # seek_sec = self.stream.frame_to_secs(self.syncedLabeledFrames[self.which_camera][self.current_frame_index])
-        seek_sec = self.stream.frame_to_secs(self.syncedLabeledFrames[self.current_trace_index][self.which_camera][self.current_frame_index]) # MODIFIED
+        seek_sec = self.stream.frame_to_secs(
+            self.syncedLabeledFrames[self.current_trace_index][
+                self.which_camera][self.current_frame_index])  # MODIFIED
         self.stream.seek(seek_sec)
 
         self.trace_done = False
@@ -83,38 +104,48 @@ class World:
             trace = self.syncedLabeledFrames[i]
             trace_reset_probs[i] = len(trace[self.which_camera])
         trace_reset_probs /= np.sum(trace_reset_probs)
-        new_trace = np.random.choice(trace_reset_probs.shape[0], 1, p=trace_reset_probs)
+        new_trace = np.random.choice(
+            trace_reset_probs.shape[0], 1, p=trace_reset_probs)
         return new_trace[0]
 
     def find_curvature_reset(self, curv_reset_probs):
-        new_sample = np.random.choice(curv_reset_probs.shape[0], 1, p=curv_reset_probs)
+        new_sample = np.random.choice(
+            curv_reset_probs.shape[0], 1, p=curv_reset_probs)
         return new_sample[0]
 
     def get_interp_functions(self):
         # Human inverse_r from filtered odometry #TODO: move to better place, all sensors should be handled in world but can be accessed from env
-        speed = np.genfromtxt(os.path.join(self.trace_path, TopicNames.speed + '.csv'), delimiter=',')
-        f_speed = interp1d(speed[:,0], speed[:,1], fill_value='extrapolate')
-        odometry = np.genfromtxt(os.path.join(self.trace_path, TopicNames.odometry + '.csv'), delimiter=',')
-        f_position = interp1d(odometry[:,0], odometry[:,1:3], axis=0, fill_value='extrapolate')
-        sample_times = odometry[:,0]
-        curvature = odometry[:,4] / np.maximum(f_speed(sample_times), 1e-10)
-        good_curvature_inds = np.abs(curvature) < 1/3.
-        f_curvature = interp1d(sample_times[good_curvature_inds], curvature[good_curvature_inds], fill_value='extrapolate')
+        speed = np.genfromtxt(
+            os.path.join(self.trace_path, TopicNames.speed + '.csv'),
+            delimiter=',')
+        f_speed = interp1d(speed[:, 0], speed[:, 1], fill_value='extrapolate')
+        odometry = np.genfromtxt(
+            os.path.join(self.trace_path, TopicNames.odometry + '.csv'),
+            delimiter=',')
+        f_position = interp1d(
+            odometry[:, 0], odometry[:, 1:3], axis=0, fill_value='extrapolate')
+        sample_times = odometry[:, 0]
+        curvature = odometry[:, 4] / np.maximum(f_speed(sample_times), 1e-10)
+        good_curvature_inds = np.abs(curvature) < 1 / 3.
+        f_curvature = interp1d(
+            sample_times[good_curvature_inds],
+            curvature[good_curvature_inds],
+            fill_value='extrapolate')
 
         return f_position, f_speed, f_curvature
 
-
     def get_curv_reset_probs(self, trace_index, f_curvature):
         # Computing probablities for resetting to places with higher curvature for current trace
-        current_timestamps = self.syncedLabeledTimestamps[self.current_trace_index]
+        current_timestamps = self.syncedLabeledTimestamps[
+            self.current_trace_index]
         # curvatures = np.abs(f_curvature(self.syncedLabeledTimestamps))
-        curvatures = np.abs(f_curvature(current_timestamps)) # MODIFIED
-        curvatures = np.clip(curvatures, 0, 1/3.)
+        curvatures = np.abs(f_curvature(current_timestamps))  # MODIFIED
+        curvatures = np.clip(curvatures, 0, 1 / 3.)
         hist, bin_edges = np.histogram(curvatures, 100, density=False)
         bins = np.digitize(curvatures, bin_edges, right=True)
         hist_density = hist / float(np.sum(hist))
         smoothing_factor = 0.001
-        curv_reset_probs = 1.0/(hist_density[bins-1]+smoothing_factor)
+        curv_reset_probs = 1.0 / (hist_density[bins - 1] + smoothing_factor)
         curv_reset_probs /= np.sum(curv_reset_probs)
 
         # ''' HARDCODING RESET TO END OF TRACE FOR TESTING PURPOSES'''
@@ -125,13 +156,12 @@ class World:
 
         return curv_reset_probs
 
-
     def get_next_valid_timestamp(self, human, desired_ego):
         first_time = self.first_time
         current_time = self.get_current_timestamp()
         index = self.current_frame_index
         time = self.get_timestamp(index)
-        human = human.copy() # dont edit the original
+        human = human.copy()  # dont edit the original
         closest_dist = float('inf')
         while True:
             next_time = self.get_timestamp(index)
@@ -140,8 +170,7 @@ class World:
             human.step(
                 curvature=self.f_curvature(time),
                 velocity=self.f_speed(time),
-                delta_t=next_time-time
-            )
+                delta_t=next_time - time)
 
             dist = np.linalg.norm(human.numpy()[:2] - desired_ego.numpy()[:2])
             if dist < closest_dist:
@@ -154,16 +183,18 @@ class World:
         closest_time = self.get_current_timestamp()
         return closest_time, last_human
 
-
     def get_frame_from_time(self, worldtime):
         # do this _once_ in the init for speed
-        frame_to_worldtime = self.masterClock.cam_to_frame_time[self.which_camera]
-        worldtime_to_frame = dict([[v,k] for k,v in frame_to_worldtime.items()])
+        frame_to_worldtime = self.masterClock.cam_to_frame_time[
+            self.which_camera]
+        worldtime_to_frame = dict(
+            [[v, k] for k, v in frame_to_worldtime.items()])
 
         desired_frame_num = worldtime_to_frame[worldtime]
 
         if desired_frame_num < self.stream.frame_num:
-            print("SEEKING SINCE {} < {}".format(desired_frame_num, self.stream.frame_num))
+            print("SEEKING SINCE {} < {}".format(desired_frame_num,
+                                                 self.stream.frame_num))
             desired_seek_sec = self.stream.frame_to_secs(desired_frame_num)
             self.stream.seek(desired_seek_sec)
 
@@ -173,18 +204,17 @@ class World:
         desired_frame = self.stream.image.copy()
         return desired_frame
 
-
     def get_timestamp(self, index):
-        if index >= len(self.syncedLabeledTimestamps[self.current_trace_index])-1:
+        if index >= len(
+                self.syncedLabeledTimestamps[self.current_trace_index]) - 1:
             print("END OF TRACE")
-            self.trace_done = True # Done var will be set to True in deepknight env step
-            return self.syncedLabeledTimestamps[self.current_trace_index][-1] # Return last timestamp
+            self.trace_done = True  # Done var will be set to True in deepknight env step
+            return self.syncedLabeledTimestamps[self.current_trace_index][
+                -1]  # Return last timestamp
         return self.syncedLabeledTimestamps[self.current_trace_index][index]
-
 
     def get_current_timestamp(self):
         return self.get_timestamp(self.current_frame_index)
-
 
     def synthesize_frame(self, frame, info, relative_state):
         translated_frame = self.view_synthesizer(
@@ -192,7 +222,6 @@ class World:
             translation_x=relative_state.translation_x,
             translation_y=relative_state.translation_y,
             image=frame)[0]
-
         translated_frame = np.uint8(translated_frame)
 
         return translated_frame
@@ -202,16 +231,18 @@ class World:
 
         # Initialize MultiFrame object
         masterClock = MultiFrame(trace_path)
-        masterTimestamp = masterClock.get_master_timestamp().reshape((-1,1))
+        masterTimestamp = masterClock.get_master_timestamp().reshape((-1, 1))
 
         # Get good frames to loop through
         goodLabeledFrames = np.ones((masterTimestamp.shape[0]), dtype=bool)
         for search_query in self.labels:
-            goodLabeledFrames *= search_query.find_good_labeled_frames(trace_path) # Logical AND via multiplication
-        syncedLabeledTimestamps = get_synced_labeled_timestamps(trace_path, goodLabeledFrames)
+            goodLabeledFrames *= search_query.find_good_labeled_frames(
+                trace_path)  # Logical AND via multiplication
+        syncedLabeledTimestamps = get_synced_labeled_timestamps(
+            trace_path, goodLabeledFrames)
 
         # Need to divide the stream into good chunks; each chunk becomes a Trace
-        syncedLabeledFrames_ = [] # FIX time
+        syncedLabeledFrames_ = []  # FIX time
         start = 0
         for i in range(len(syncedLabeledTimestamps)):
             # CHECK:
@@ -219,11 +250,12 @@ class World:
             if syncedLabeledTimestamps[i] == syncedLabeledTimestamps[-1] or \
                 syncedLabeledTimestamps[i+1] - syncedLabeledTimestamps[i] >= .2 :
                 print("SPAWN: FOUND NEW TRACE")
-                syncedLabeledFrames = masterClock.get_frames_from_times(syncedLabeledTimestamps[start:i])
+                syncedLabeledFrames = masterClock.get_frames_from_times(
+                    syncedLabeledTimestamps[start:i])
                 frames = {}
                 frames['camera_front'] = syncedLabeledFrames['camera_front']
                 syncedLabeledFrames_.append(frames)
-                start = i+1
+                start = i + 1
                 break
 
         # TODO: Ensure other components expect list of dictionaries
