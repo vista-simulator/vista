07882ef7bfb61aff1bfa6ced5528f84888ffc9d4
Tsun-Hsuan Wang <tsunw@mit.edu>
2021-04-25 17:30:06 -0400

    add bev map ob

--- learning/envs/state_obs.py
+++ learning/envs/state_obs.py
@@ -4,6 +4,10 @@ from functools import partial
 import gym
 import pickle5 as pickle
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from shapely.geometry import LineString
+from descartes import PolygonPatch
 import ray
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
 
@@ -14,11 +18,12 @@ from . import *
 def StateObs(task, **kwargs):
     task = globals()[task]
     class _StateObs(task, MultiAgentEnv):
-        def __init__(self, aug_extra_obs=False, **kwargs):
+        def __init__(self, aug_extra_obs=False, to_bev_map=False, **kwargs):
             self.drop_obs_space_def = True
             super(_StateObs, self).__init__(**kwargs)
 
             self.aug_extra_obs = aug_extra_obs
+            self.to_bev_map = to_bev_map
 
             controllable_agent_ids = list(self.controllable_agents.keys())
             assert len(controllable_agent_ids) == 1 and controllable_agent_ids[0] == self.ref_agent_id, \
@@ -28,29 +33,69 @@ def StateObs(task, **kwargs):
             if self.require_handling_road:
                 self.init_scene_state(200)
 
+            # define observation space
             self.fake_cam_height, self.fake_cam_width = 10, 15
             self.render_observation_space = gym.spaces.Box(
                 low=0,
                 high=255,
                 shape=(self.fake_cam_height, self.fake_cam_width, 3),
                 dtype=np.uint8)
-            obs_size = self.road_buffer_size * 2 + 5 * self.n_agents # road xy and agent xytheta + velocity + curvature
-            if self.aug_extra_obs and hasattr(self, 'extra_obs'):
-                obs_size += self.extra_obs.shape[0]
-            self.observation_space = gym.spaces.Box(
-                low=-100., # NOTE: hardcoded bound for birdseye map range
-                high=100.,
-                shape=(obs_size,),
-                dtype=np.float64)
+            if self.to_bev_map:
+                figsize_in_pix = (80, 120) # 4x smaller
+
+                self.observation_space = gym.spaces.Box(
+                    low=0,
+                    high=255,
+                    shape=(figsize_in_pix[1], figsize_in_pix[0], 3),
+                    dtype=np.uint8)
+            else:
+                obs_size = self.road_buffer_size * 2 + 5 * self.n_agents # road xy and agent xytheta + velocity + curvature
+                if self.aug_extra_obs and hasattr(self, 'extra_obs'):
+                    obs_size += self.extra_obs.shape[0]
+                self.observation_space = gym.spaces.Box(
+                    low=-100., # NOTE: hardcoded bound for birdseye map range
+                    high=100.,
+                    shape=(obs_size,),
+                    dtype=np.float64)
 
             if not hasattr(self, 'wrap_data'):
                 self.wrap_data = lambda _x: _x
 
             self.vehicle_states = {k: None for k in self.agent_ids}
 
+            if self.to_bev_map:
+                # initialize plot
+                colors = list(cm.get_cmap('Set1').colors)
+                rgba2rgb = lambda rgba: np.clip((1 - rgba[:3]) * rgba[3] + rgba[:3], 0., 1.)
+                colors = [np.array(list(c) + [0.6]) for c in colors]
+                colors = list(map(rgba2rgb, colors))
+                self.road_color = list(cm.get_cmap('Dark2').colors)[-1]
+                self.agent_colors = colors
+
+                self.artists = dict()
+                n_agents_with_sensor = 0
+                px = 1/plt.rcParams['figure.dpi']  # pixel in inches
+                figsize = (figsize_in_pix[0]*px, figsize_in_pix[1]*px)
+                self.fig = plt.figure(figsize=figsize)
+                self.fig.subplots_adjust(left=0.01, right=0.98, bottom=0.02, top=0.90, wspace=0.03, hspace=0.03)
+                self.fig.patch.set_facecolor('black') # use black background
+                self.gs = self.fig.add_gridspec(5, 4 * n_agents_with_sensor + 2)
+
+                # birds eye map
+                self.ax_birdseye = self.fig.add_subplot(self.gs[:,-2:])
+                self.ax_birdseye.set_facecolor('black')
+                self.ax_birdseye.set_xticks([])
+                self.ax_birdseye.set_yticks([])
+                self.birdseye_map_size = (30, 20)
+
         def reset(self, **kwargs):
             # reset base env
             observation = super().reset()
+
+            if self.to_bev_map:
+                # set birds eye map size
+                self.ax_birdseye.set_xlim(-self.birdseye_map_size[1]/2., self.birdseye_map_size[1]/2.)
+                self.ax_birdseye.set_ylim(-self.birdseye_map_size[0]/2., self.birdseye_map_size[0]/2.)
 
             # get state observation
             if self.require_handling_road:
@@ -60,7 +105,7 @@ def StateObs(task, **kwargs):
             for agent_id, agent in zip(self.agent_ids, self.world.agents):
                 self.vehicle_states[agent_id] = np.array([get_human_curvature(agent)[0], get_human_speed(agent)[0]])
             observation = self.get_state_obs()
-            if self.aug_extra_obs and hasattr(self, 'extra_obs'):
+            if not self.to_bev_map and (self.aug_extra_obs and hasattr(self, 'extra_obs')):
                 observation[self.ref_agent_id] = np.concatenate([observation[self.ref_agent_id], self.extra_obs])
 
             return observation
@@ -72,39 +117,70 @@ def StateObs(task, **kwargs):
             for agent_id, agent in zip(self.agent_ids, self.world.agents):
                 self.vehicle_states[agent_id] = np.array([agent.model_curvature, agent.model_velocity])
             observation = self.get_state_obs()
-            if self.aug_extra_obs and hasattr(self, 'extra_obs'):
+            if not self.to_bev_map and (self.aug_extra_obs and hasattr(self, 'extra_obs')):
                 observation[self.ref_agent_id] = np.concatenate([observation[self.ref_agent_id], self.extra_obs])
             done['__all__'] = done_all
             return observation, reward, done, info
 
         def get_state_obs(self):
-            # get scene state
             ref_dynamics = self.ref_agent.human_dynamics
             road_in_ref, ego_in_ref = self.get_scene_state(concat=False, ref_dynamics=ref_dynamics)
-            ego_in_ref = np.concatenate([ego_in_ref, self.vehicle_states[self.ref_agent_id]])
-            others_in_ref = []
-            for agent_idx, agent in enumerate(self.world.agents):
-                if agent_idx == self.ref_agent_idx:
-                    continue
-                other_in_ref = self.compute_relative_transform(agent.ego_dynamics, ref_dynamics)
-                other_in_ref = np.concatenate([other_in_ref, self.vehicle_states[self.agent_ids[agent_idx]]])
-                others_in_ref.append(other_in_ref)
 
-            # prepare observation
-            aug_road_in_ref = np.concatenate([np.zeros(\
-                (self.road_buffer_size-road_in_ref[:,:2].shape[0],2)), road_in_ref[:,:2]]) # NOTE: drop theta state
-            state_obs = np.concatenate([aug_road_in_ref.reshape((-1,)), ego_in_ref] + others_in_ref)
-            observation = {self.ref_agent_id: state_obs}
+            if self.to_bev_map:
+                # update road in bev map
+                patch = LineString(road_in_ref).buffer(self.ref_agent.trace.road_width / 2.)
+                patch = PolygonPatch(patch, fc=self.road_color, ec=self.road_color, zorder=1)
+                self.update_patch(self.ax_birdseye, 'patch:road', patch)
 
-            for k in self.observation_for_render.keys():
-                self.observation_for_render[k] = np.zeros(self.render_observation_space.shape,\
-                    dtype=self.render_observation_space.dtype)
+                # update agent in birds eye map (in reference agent coordinate)
+                for i, (agent_id, agent) in enumerate(zip(self.agent_ids, self.world.agents)):
+                    poly = self.agent2poly(agent, self.ref_agent.human_dynamics)
+                    patch = PolygonPatch(poly, fc=self.agent_colors[i], ec=self.agent_colors[i], zorder=2)
+                    self.update_patch(self.ax_birdseye, 'patch:{}'.format(agent_id), patch)
+
+                # get observation
+                observation = {self.ref_agent_id: self.fig2img(self.fig)}
+            else:
+                # get scene state
+                ego_in_ref = np.concatenate([ego_in_ref, self.vehicle_states[self.ref_agent_id]])
+                others_in_ref = []
+                for agent_idx, agent in enumerate(self.world.agents):
+                    if agent_idx == self.ref_agent_idx:
+                        continue
+                    other_in_ref = self.compute_relative_transform(agent.ego_dynamics, ref_dynamics)
+                    other_in_ref = np.concatenate([other_in_ref, self.vehicle_states[self.agent_ids[agent_idx]]])
+                    others_in_ref.append(other_in_ref)
+
+                # prepare observation
+                aug_road_in_ref = np.concatenate([np.zeros(\
+                    (self.road_buffer_size-road_in_ref[:,:2].shape[0],2)), road_in_ref[:,:2]]) # NOTE: drop theta state
+                state_obs = np.concatenate([aug_road_in_ref.reshape((-1,)), ego_in_ref] + others_in_ref)
+                observation = {self.ref_agent_id: state_obs}
+
+                for k in self.observation_for_render.keys():
+                    self.observation_for_render[k] = np.zeros(self.render_observation_space.shape,\
+                        dtype=self.render_observation_space.dtype)
             self.observation_for_render = self.wrap_data(self.observation_for_render)
 
             return observation
 
         def agent_sensors_setup(self, agent_i):
             pass # don't need sensor; use ground truth state
+
+        def fig2img(self, fig):
+            fig.canvas.draw()
+            buf = fig.canvas.buffer_rgba()
+            img = np.asarray(buf)[:,:,:3]
+            return img
+
+        def update_patch(self, ax, name, patch):
+            if name in self.artists:
+                self.artists[name].remove()
+            ax.add_patch(patch)
+            self.artists[name] = patch
+
+        def obs_for_render(self, obs):
+            return obs
 
     return _StateObs(**kwargs)
 
@@ -132,10 +208,15 @@ if __name__ == "__main__":
         type=str,
         default=None,
         help='Task')
+    parser.add_argument(
+        '--to-bev-map',
+        default=False,
+        action='store_true',
+        help='Convert state observation to bev map.')
     args = parser.parse_args()
 
     # initialize simulator
-    env = StateObs(args.task, trace_paths=args.trace_paths, mesh_dir=args.mesh_dir)
+    env = StateObs(args.task, trace_paths=args.trace_paths, mesh_dir=args.mesh_dir, to_bev_map=args.to_bev_map)
     env = MultiAgentMonitor(env, os.path.expanduser('~/tmp/monitor'), video_callable=lambda x: True, force=True)
 
     # run
