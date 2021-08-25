0405964482077f1bd169ba1e3471928219c4f2fe
Johnson Wang <johnsonwang0810@gmail.com>
2021-08-25 16:16:12 -0400

    dev buffered shuffle streaming dataloader

--- gpl/il_minimal.py
+++ gpl/il_minimal.py
@@ -3,6 +3,7 @@ import os
 import time
 import numpy as np
 import argparse
+import random
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -21,7 +22,7 @@ class VistaDataset(IterableDataset):
     def __init__(self, trace_paths, transform, train=False):
         trace_config = dict(
             road_width=4,
-            reset_mode='segment_start',
+            reset_mode='default',
             master_sensor='front_center',
         )
         car_config = dict(
@@ -49,20 +50,44 @@ class VistaDataset(IterableDataset):
         self.transform = transform
         self.train = train
 
+        self.buffer_size = 1000 # DEBUG
+
         # NOTE: just to make len() still work in the master process; will be instantiated 
         # independently for each worker in worker_init_fn
         self.world = vista.World(trace_paths, trace_config)
 
     def __len__(self):
-        return np.sum([tr.num_of_frames for tr in self.world.traces])
+        return 6000 # DEBUG np.sum([tr.num_of_frames for tr in self.world.traces])
 
     def __iter__(self):
-        return self._simulate()
+        buf = []
+        for x in self._simulate():
+            if len(buf) == self.buffer_size:
+                idx = random.randint(0, self.buffer_size - 1)
+                yield buf[idx]
+                buf[idx] = x
+            else:
+                buf.append(x)
+        random.shuffle(buf)
+        while buf:
+            yield buf.pop()
 
     def _simulate(self):
+        # initialization for num_worker = 0
+        worker_info = torch.utils.data.get_worker_info()
+        if worker_info is None:
+            self.agent = self.world.spawn_agent(self.car_config)
+            self.camera = self.agent.spawn_camera(self.camera_config)
+            self.world.reset()
+
+        # data generator from simulation
+        [self.world.reset() for _ in range(worker_info.id)] # hacky way to make different workers reset differently
         while True:
             if self.agent.done:
-                self.world.reset()
+                # self.world.reset()
+                [self.world.reset() for _ in range(worker_info.id)]
+
+            print(worker_info.id, self.agent.trace_index, self.agent.segment_index, self.agent.frame_index) # DEBUG
 
             self.agent.step_dataset(step_dynamics=False)
             sensor_name = self.agent.sensors[0].name
@@ -71,10 +96,17 @@ class VistaDataset(IterableDataset):
             (i1, j1, i2, j2) = self.agent.sensors[0].camera_param.get_roi()
             img = img[i1:i2, j1:j2]
 
-            img = self.transform(img)
+            img = standardize(self.transform(img))
             label = np.array([self.agent.human_curvature]).astype(np.float32)
 
             yield img, label
+
+
+def standardize(x):
+    # follow https://www.tensorflow.org/api_docs/python/tf/image/per_image_standardization
+    mean, stddev = x.mean(), x.std()
+    adjusted_stddev = max(stddev, 1.0/np.sqrt(np.prod(x.shape)))
+    return (x - mean) / adjusted_stddev
 
 
 class Net(nn.Module):
@@ -98,13 +130,15 @@ class Net(nn.Module):
             nn.ReLU(inplace=True)
         )
         self.policy = nn.Sequential(
-            nn.Linear(1280, 64),
-            nn.BatchNorm1d(64),
+            nn.Linear(1280, 1000),
+            # nn.BatchNorm1d(1000),
             nn.ReLU(inplace=True),
-            nn.Linear(64, 32),
-            nn.BatchNorm1d(32),
+            nn.Dropout(p=0.3),
+            nn.Linear(1000, 100),
+            # nn.BatchNorm1d(100),
             nn.ReLU(inplace=True),
-            nn.Linear(32, 1)
+            nn.Dropout(p=0.3),
+            nn.Linear(100, 1)
         )
 
     def forward(self, x):
@@ -118,33 +152,46 @@ class Net(nn.Module):
 def train(args, model, device, train_loader, criterion, optimizer, epoch):
     model.train()
     tic = time.time()
+    sample_i = 0
+    total_samples = len(train_loader.dataset)
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = criterion(output, target) / len(data)
+        loss = 10e4 * criterion(output, target) / len(data)
         loss.backward()
         optimizer.step()
         if batch_idx % 10 == 0:
             toc = time.time()
             elapsed_time = toc - tic
             curr_n_samples = batch_idx * len(data)
-            total_samples = len(train_loader.dataset)
             progress = 100. * batch_idx / len(train_loader)
             print(f'Training epoch {epoch} [{curr_n_samples}/{total_samples} ({progress:.2f}%)]' \
                   + f'\t Loss: {loss.item():.6f}\t Elapsed time: {elapsed_time:.2f}')
             tic = toc
+        
+        sample_i += len(data)
+        if sample_i >= total_samples:
+            break
 
 
 def test(model, device, test_loader, criterion):
     model.eval()
     test_loss = 0
+    sample_i = 0
+    total_samples = len(test_loader.dataset)
     tic = time.time()
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
+    for data, target in test_loader:
+        data, target = data.to(device), target.to(device)
+        with torch.no_grad():
             output = model(data)
-            test_loss += criterion(output, target).item()
+            test_loss += 10e4 * criterion(output, target).item()
+
+        print(output.item(), target.item()) # DEBUG
+
+        sample_i += len(data)
+        if sample_i >= total_samples:
+            break
     toc = time.time()
 
     elapsed_time = toc - tic
@@ -195,10 +242,10 @@ def main():
     train_trace_paths = [os.path.join(data_dir, 'traces', v) for v in train_trace_paths]
     train_dataset = VistaDataset(train_trace_paths, train_transform, train=True)
     train_loader = DataLoader(train_dataset,
-                              batch_size=64,
+                              batch_size=1,
                               shuffle=False, # NOTE: IterDataset doesn't allow shuffle
                               pin_memory=True,
-                              num_workers=8,
+                              num_workers=4,
                               drop_last=True,
                               worker_init_fn=worker_init_fn)
 
@@ -212,16 +259,16 @@ def main():
     test_trace_paths = [os.path.join(data_dir, 'traces', v) for v in test_trace_paths]
     test_dataset = VistaDataset(test_trace_paths, test_transform, train=False)
     test_loader = DataLoader(test_dataset,
-                             batch_size=64,
+                             batch_size=1,
                              shuffle=False,
                              pin_memory=True,
-                             num_workers=8,
+                             num_workers=0,
                              drop_last=True,
                              worker_init_fn=worker_init_fn)
 
     # Define model and optimizer
     model = Net().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=0.0002)
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
     criterion = nn.MSELoss(reduction='sum')
 
     # Run training
@@ -231,7 +278,7 @@ def main():
         test_loss = test(model, device, test_loader, criterion)
         all_test_loss.append(test_loss)
         if epoch % 1 == 0:
-            save_dir = os.environ.get('RESULT_DIR', './ckpt/il')
+            save_dir = os.environ.get('RESULT_DIR', './ckpt/il2')
             if not os.path.isdir(save_dir):
                 os.makedirs(save_dir)
             torch.save(model.state_dict(), os.path.join(save_dir, 'ep_{:03d}.ckpt'.format(epoch)))
